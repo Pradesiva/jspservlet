6447d5db3c5379f9ae2d266b79976d4f71e799ac
pradeesha <168086849+Pradesiva@users.noreply.github.com>
2024-09-27 20:53:13 +0530

    Add files via upload

---
+++ jsp/Main/person.java
@@ -0,0 +1,27 @@
+package springdemo;
+
+public class Person {
+	int id;
+	String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	//public Person(int id, String name) {
+		//super();
+		//this.id = id;
+		//this.name = name;
+//}
+	}
+	
+	
+	
+	
